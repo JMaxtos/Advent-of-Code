e8ab49550ee5dbdd7ece7cab71b68feac59917d4
Joel Matos <jmmso4@iscte-iul.pt>
2024-12-09 23:00:47 +0000

    Day2 part1 improvement

--- src/main/java/aoc2024/day2/Day2_part1.java
+++ src/main/java/aoc2024/day2/Day2_part1.java
@@ -1,8 +1,6 @@
 package aoc2024.day2;
 
 public class Day2_part1 extends Day2 {
-    private final int INCREASING = 1;
-    private final int DECREASING = 0;
 
     public Day2_part1() {
         super();
@@ -12,30 +10,29 @@ public class Day2_part1 extends Day2 {
 
     private int checkLevels() {
         int safeReports = 0;
-        int i = 0;
         for (int[] report : levels) {
             if (report == null) {
                 continue;
             }
-            i++;
-            if (isSafeReport(report, i)) {
+            if (isSafeReport(report)) {
                 safeReports++;
             }
         }
         return safeReports;
     }
 
-    private boolean isSafeReport(int[] report, int level) {
+    private boolean isSafeReport(int[] report) {
         int[] validate = new int[report.length - 1];
         for (int i = 0; i < report.length - 1; i++) {
             if (Math.abs(report[i] - report[i + 1]) > 3) {
                 return false;
             } else {
+
                 if (report[i] > report[i + 1] && i != 0 && validate[i - 1] == INCREASING) {
-                    return false;
+                   return false;
                 }
                 if (report[i] < report[i + 1] && i != 0 && validate[i - 1] == DECREASING) {
-                    validate[i] = DECREASING;
+                    return false;
                 }
                 if (report[i] < report[i + 1]) {
                     validate[i] = INCREASING;
@@ -48,11 +45,8 @@ public class Day2_part1 extends Day2 {
                 }
             }
         }
-        for (int i = 1; i < validate.length; i++) {
-            if (validate[i] != validate[i - 1]) {
-                return false;
-            }
-        }
+
         return true;
     }
+
 }
---
+++ src/main/java/aoc2024/day2/Main_part2.java
@@ -0,0 +1,7 @@
+package aoc2024.day2;
+
+public class Main_part2 {
+    static public void main(String[] args) {
+        Day2_part2 d = new Day2_part2();
+    }
+}
