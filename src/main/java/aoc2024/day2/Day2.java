1179d9b83dc2ce5865dc952ccf77f625a3766308
Joel Matos <jmmso4@iscte-iul.pt>
2024-12-09 20:16:23 +0000

    Day 2 part 1 solved

--- src/main/java/aoc2024/day2/Day2.java
+++ src/main/java/aoc2024/day2/Day2.java
@@ -1,44 +1,43 @@
 package aoc2024.day2;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Day2 {
-    public int[][] levels;
+    protected List<int[]> levels;
 
     public Day2() {
-        levels = new int[1000][];
+        levels = new ArrayList<int[]>();
         readValues();
     }
 
     private void readValues() {
         Scanner sc = new Scanner(System.in);
-        int i = 0;
+
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
             if (!line.trim().isEmpty()) {
                 String[] parts = line.trim().split("\\s+");
-                levels[i] = new int[parts.length];
-                int j = 0;
-                while (j < parts.length) {
-                    levels[i][j] = Integer.parseInt(parts[j]);
-                    j++;
+                int[] reports = new int[parts.length];
+                for (int i = 0; i < reports.length; i++) {
+                    reports[i] = Integer.parseInt(parts[i]);
                 }
+                levels.add(reports);
             } else {
                 break;
             }
-            i++;
         }
         sc.close();
     }
 
+    //to String Method to make sure everything was getting read
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < levels.length; i++) {
-            for (int j = 0; j < levels[i].length; j++) {
-                sb.append(levels[i][j]);
-                sb.append(" ");
+        for (int[] report : levels) {
+            for (int num : report) {
+                sb.append(num).append(" ");
             }
             sb.append("\n");
         }
--- src/main/java/aoc2024/day2/Day2_part1.java
+++ src/main/java/aoc2024/day2/Day2_part1.java
@@ -1,4 +1,58 @@
 package aoc2024.day2;
 
-public class Day2_part1 {
+public class Day2_part1 extends Day2 {
+    private final int INCREASING = 1;
+    private final int DECREASING = 0;
+
+    public Day2_part1() {
+        super();
+        int safeReports = checkLevels();
+        System.out.println("Number of safe reports: " + safeReports);
+    }
+
+    private int checkLevels() {
+        int safeReports = 0;
+        int i = 0;
+        for (int[] report : levels) {
+            if (report == null) {
+                continue;
+            }
+            i++;
+            if (isSafeReport(report, i)) {
+                safeReports++;
+            }
+        }
+        return safeReports;
+    }
+
+    private boolean isSafeReport(int[] report, int level) {
+        int[] validate = new int[report.length - 1];
+        for (int i = 0; i < report.length - 1; i++) {
+            if (Math.abs(report[i] - report[i + 1]) > 3) {
+                return false;
+            } else {
+                if (report[i] > report[i + 1] && i != 0 && validate[i - 1] == INCREASING) {
+                    return false;
+                }
+                if (report[i] < report[i + 1] && i != 0 && validate[i - 1] == DECREASING) {
+                    validate[i] = DECREASING;
+                }
+                if (report[i] < report[i + 1]) {
+                    validate[i] = INCREASING;
+                }
+                if (report[i] > report[i + 1]) {
+                    validate[i] = DECREASING;
+                }
+                if (report[i] == report[i + 1]) {
+                    return false;
+                }
+            }
+        }
+        for (int i = 1; i < validate.length; i++) {
+            if (validate[i] != validate[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
