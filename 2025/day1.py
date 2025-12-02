c2966ec757313772fcf7e7d855cdbf64cee1e573
JMaxtos <jmmso4@iscte-iul.pt>
2025-12-02 12:50:44 +0000

    Day 1 Complete!

---
+++ 2025/day1.py
@@ -0,0 +1,56 @@
+INITIAL_POITING = 50
+MAX_POINTING = 99
+MIN_POITING = 0
+
+
+
+FILENAME = input("Insert input filename (*.txt): " )
+file = open(FILENAME)
+
+def partOne(file):
+    password = 0
+    currentPoiting=INITIAL_POITING
+
+    for line in file:
+        direction = line[0]
+        numRotations = int(line[1:])
+
+        if direction == 'L':
+            currentPoiting = (currentPoiting - numRotations) % 100
+        
+        if direction == 'R':
+            currentPoiting = (currentPoiting + numRotations)%100
+        
+        if currentPoiting == 0 :
+            password+=1
+
+        print(f"Current Poiting to: {currentPoiting}")
+
+    print(f"Password:{password}")
+
+#partOne(file)
+
+
+def partTwo(file):
+    password = 0
+
+    currentPoiting=INITIAL_POITING
+    previousPoiting = INITIAL_POITING
+
+    for line in file:
+        direction = line[0]
+        numRotations = int(line[1:])
+        
+        for _ in range(numRotations):
+            if direction == 'L':
+                currentPoiting = (currentPoiting-1) % 100
+            elif direction == 'R':
+                currentPoiting = (currentPoiting + 1) % 100
+
+            if currentPoiting == MIN_POITING:
+                password+=1
+    print(f"Password:{password}")
+
+     
+
+partTwo(file)
