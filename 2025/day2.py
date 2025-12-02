d812581ee73a150bc627cd0e6885b9cf75248e2d
JMaxtos <jmmso4@iscte-iul.pt>
2025-12-02 13:58:15 +0000

    Day 2 complete

--- 2025/day2.py
+++ 2025/day2.py
@@ -1,15 +1,12 @@
 
-FILENAME = input("Insert name File (.txt): ")
-file = open(FILENAME)
-
 
 def partOne(file):
     invalidIDs = 0
     idRanges = []
+    values = []
     for line in file:
         values = line.split(',')
     
-   
     for value in values:
         min,max = value.split('-')
         idRanges.append((int(min),int(max)))
@@ -27,4 +24,40 @@ def partOne(file):
 
 
             
+
+
+def partTwo(file):
+    invalidIDs = 0
+    idRanges = []
+    values = []
+    for line in file:
+        values = line.split(',')
+
+    for value in values:
+        min,max = value.split('-')
+        idRanges.append((int(min),int(max)))
+        
+    
+    for min,max in idRanges:
+        for number in range(min,max+1):
+            numberString = str(number)
+            for size in range(1,len(numberString)):
+                if len(numberString)%size != 0:
+                    continue
+                numSequenceRepeats = len(numberString)//size
+                if numSequenceRepeats < 2:
+                    continue
+                sequenceBaseNumber = numberString[:size]
+                if sequenceBaseNumber * numSequenceRepeats == numberString:
+                    invalidIDs += number
+                    break
+
+           
+    return invalidIDs
+
+FILENAME = input("Insert name File (.txt): ")
+file = open(FILENAME)
+
 print(f"Part One Result: {partOne(file)}")
+file = open(FILENAME)
+print(f"Part Two Result: {partTwo(file)}")
