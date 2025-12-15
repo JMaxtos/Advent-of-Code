4df128d82ae5882952a5f91ebc900b2b430fb3fc
JMaxtos <jmmso4@iscte-iul.pt>
2025-12-15 01:14:49 +0000

    Day 5 Complete

--- 2025/day5.py
+++ 2025/day5.py
@@ -8,7 +8,7 @@ def readFiles():
     valuesFile = open(VALUES_FILENAME)
     for line in file:
         interval = line.split('-')
-        fileLines.append((interval[0].strip(),interval[1].strip()))
+        fileLines.append((int(interval[0]),int(interval[1].strip())))
     for line in valuesFile:
         fileTestValues.append(line.strip())
     return fileLines, fileTestValues
@@ -25,7 +25,26 @@ def partOne():
 def isFresh(min,max,value):
     return int(min) <=  int(value) <= int(max) 
 
+def partTwo():
+    fileLines, _ = readFiles()
+    fileLines.sort(key=lambda x: x[0])
+
+    merged = []
+    for interval in fileLines:
+        if not merged:
+            merged.append([interval[0],interval[1]])
+        else:
+            last_min,last_max = merged[-1]
+            if interval[0] <= last_max +1:
+                merged[-1][1]= max(last_max,interval[1])
+            else:
+                merged.append([interval[0],interval[1]])
+    freshIds =sum(max-min + 1 for min,max in merged )
+    return freshIds
+    
+
 
 
 print(f"Part One : {partOne()}")
+print(f"Part Two : {partTwo()}")
     
